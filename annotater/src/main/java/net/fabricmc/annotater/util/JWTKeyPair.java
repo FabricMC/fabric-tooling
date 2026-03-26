440f9b7b8e7445bdddaff4ca7a6322adbfe83429
modmuss50 <modmuss50@gmail.com>
2026-03-26 12:49:40 +0000

    Improve PEM handling by using new Java API

--- annotater/src/main/java/net/fabricmc/annotater/util/JWTKeyPair.java
+++ annotater/src/main/java/net/fabricmc/annotater/util/JWTKeyPair.java
@@ -1,16 +1,15 @@
 package net.fabricmc.annotater.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.KeyFactory;
+import java.security.DEREncodable;
 import java.security.NoSuchAlgorithmException;
+import java.security.PEMDecoder;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.ECDSAKeyProvider;
@@ -49,38 +48,28 @@ public class JWTKeyPair implements ECDSAKeyProvider {
 
 	// Load an EC private key from a PEM file
 	public static ECPrivateKey loadPrivateKey(Path path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-		byte[] keyBytes = Files.readAllBytes(path);
+		DEREncodable decoded = decodePEM(path);
 
-		String pem = new String(keyBytes);
-
-		if (pem.contains("BEGIN")) {
-			pem = pem
-					.replace("-----BEGIN PRIVATE KEY-----", "")
-					.replace("-----END PRIVATE KEY-----", "")
-					.replaceAll("\\s", "");
-			keyBytes = Base64.getDecoder().decode(pem);
+		if (decoded instanceof ECPrivateKey privateKey) {
+			return privateKey;
 		}
 
-		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
-		KeyFactory kf = KeyFactory.getInstance("EC");
-		return (ECPrivateKey) kf.generatePrivate(spec);
+		throw new IllegalArgumentException("Unsupported PEM type: " + decoded.getClass().getName());
 	}
 
 	public static ECPublicKey loadECPublicKey(Path path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-		byte[] keyBytes = Files.readAllBytes(path);
+		DEREncodable decoded = decodePEM(path);
 
-		String pem = new String(keyBytes);
-
-		if (pem.contains("BEGIN")) {
-			pem = pem
-					.replace("-----BEGIN PUBLIC KEY-----", "")
-					.replace("-----END PUBLIC KEY-----", "")
-					.replaceAll("\\s", "");
-			keyBytes = Base64.getDecoder().decode(pem);
+		if (decoded instanceof ECPublicKey publicKey) {
+			return publicKey;
 		}
 
-		X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
-		KeyFactory kf = KeyFactory.getInstance("EC");
-		return (ECPublicKey) kf.generatePublic(spec);
+		throw new IllegalArgumentException("Unsupported PEM type: " + decoded.getClass().getName());
+	}
+
+	private static DEREncodable decodePEM(Path path) throws IOException {
+		try (InputStream is = Files.newInputStream(path)) {
+			return PEMDecoder.of().decode(is);
+		}
 	}
 }
